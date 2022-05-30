4518914823826983299c4bdbc11b3c6fbb44d3d9
Deo G <66809588+921Mdas@users.noreply.github.com>
2022-05-30 18:27:12 -0400

    exact

--- src/context/type.js
+++ src/context/type.js
@@ -15,15 +15,15 @@ export const CHANGE_LAYOUT = "CHANGE_LAYOUT";
 const BASEURL = "http://localhost:3001/";
 
 // API URLS
-export const GET_CONTENT_URL = ` ${BASEURL}api/articles/content`;
-export const LOADMORE_URL = `${BASEURL}api/articles/loadmore`;
-export const DELETE_ARTICLE_URL = `${BASEURL}api/articles/admin/delete/`;
-export const LIKE_ARTICLE_URL = `${BASEURL}api/articles/admin/like/`;
-export const GETLIKED_ARTICLE_URL = `${BASEURL}api/articles/admin/like/`;
-export const SIGN_UP_URL = `${BASEURL}api/users/register`;
-export const SIGN_IN_URL = `${BASEURL}api/users/signin`;
-export const AUTO_SIGN_URL = `${BASEURL}api/users/isauth`;
-export const CREATE_ARTICLE_URL = `${BASEURL}api/articles/admin/addarticles`;
+export const GET_CONTENT_URL = ` /api/articles/content`;
+export const LOADMORE_URL = `/api/articles/loadmore`;
+export const DELETE_ARTICLE_URL = `/api/articles/admin/delete/`;
+export const LIKE_ARTICLE_URL = `/api/articles/admin/like/`;
+export const GETLIKED_ARTICLE_URL = `/api/articles/admin/like/`;
+export const SIGN_UP_URL = `/api/users/register`;
+export const SIGN_IN_URL = `/api/users/signin`;
+export const AUTO_SIGN_URL = `/api/users/isauth`;
+export const CREATE_ARTICLE_URL = `/api/articles/admin/addarticles`;
 
 // local Storage item names
 export const ARTICLES_STORED = "articles";
